f8e8a1b330e32fcd575810ec7daadd727f36ec75
Nfor Nde Nyambi <xinodeprinz@gmail.com>
2024-06-16 06:51:04 +0100

    Update CandidateController.php

--- app/Http/Controllers/CandidateController.php
+++ app/Http/Controllers/CandidateController.php
@@ -40,10 +40,10 @@ class CandidateController extends Controller
 
         // Elections data
         $electionsData = [
-            deptStartTime => "2024-06-16 06:19:00",
-            facultyStartTime => "2024-06-17 14:29:00",
-            duration => 90, //In minutes
-            positionDuration => 1, //In minutes
+            "deptStartTime" => "2024-06-16 06:19:00",
+            "facultyStartTime" => "2024-06-17 14:29:00",
+            "duration" => 90, //In minutes
+            "positionDuration" => 1, //In minutes
         ];
 
         return Inertia::render('elections', compact(
