5325eb012adad6603c221b06afc89ac56d28cfce
Nfor Nde Nyambi <xinodeprinz@gmail.com>
2024-07-24 12:14:34 +0100

    Update CandidateController.php

--- app/Http/Controllers/CandidateController.php
+++ app/Http/Controllers/CandidateController.php
@@ -40,8 +40,8 @@ class CandidateController extends Controller
 
         // Elections data
         $electionsData = [
-            "deptStartTime" => "2024-07-24 12:13:00",
-            "facultyStartTime" => "2024-07-17 15:30:00",
+            "deptStartTime" => "2024-07-24 12:15:00",
+            "facultyStartTime" => "2024-07-24 15:30:00",
             "duration" => 90, //In minutes
             "positionDuration" => 1, //In minutes
         ];
